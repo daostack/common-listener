401ba4f5787bab53f12327ba709438bdbf14d4f4
jelle <jellegerbrandy@gmail.com>
2020-05-31 12:07:04 +0200

    remove useless code, simple refactor

--- index.js
+++ index.js
@@ -10,74 +10,39 @@ const arc = new Arc({
   graphqlWsProvider: graphwsLink,
 });
 
-function startDAOSubscription() {
+function subscribeToCommons() {
   arc
-    .daos({}, {subscribe: true, fetchAllData: true})
+    .daos({}, { subscribe: true, fetchAllData: true })
     .subscribe(async () => {
       const url = `${CLOUDFUNCTIONS_URL}update-daos`;
       const request = await fetch(url);
       if (request.status !== 200) {
         throw Error(`Error fetching ${url}: ${request.status} ${request.statusText}`);
       }
-      console.log('Updated DAOs: ', request.status, request.statusText);
+      console.log(Date(), 'Updated DAOs: ', request.status, request.statusText);
     });
 }
 
-startDAOSubscription();
 
-arc.proposals({}, {subscribe: true, fetchAllData: true})
-  .subscribe(async proposals => {
-    proposals.map(proposal => {
-      const {coreState} = proposal;
-      const proposalObject = {
+function subscribeToProposals() {
+  arc.proposals({}, { subscribe: true, fetchAllData: true })
+    .subscribe(async () => {
+      try {
 
-        boostedAt: coreState.boostedAt,
-        createdAt: coreState.createdAt,
-        dao: coreState.dao.id,
-        description: coreState.description,
-        expiresInQueueAt: coreState.expiresInQueueAt,
-        executionState: coreState.executionState,
-        executed: coreState.executed,
-        funding: coreState.funding.toString(),
-        executedAt: coreState.executedAt,
-        joinAndQuit: {
-          funding: coreState.funding.toString(),
-          proposedMemberId: coreState.proposer,
-          proposedMemberAddress: coreState.proposer,
-        },
-        preBoostedAt: coreState.preBoostedAt,
-        id: coreState.id,
-        name: coreState.name,
-        proposer: coreState.proposer,
-        proposerId: coreState.proposer,
-        resolvedAt: coreState.resolvedAt,
-        stage: coreState.stage,
-        title: coreState.title,
-        type: coreState.type,
-        votes: {
-          list: coreState.votes,
-          votesAgainst: parseInt(coreState.votesAgainst),
-          votesFor: parseInt(coreState.votesFor),
-        },
-        links: [{
-          title: "website",
-          url: coreState.url,
-        }],
-        images: [
-        ],
-        winningOutcome: coreState.winningOutcome,
-      };
-      console.log('PROPOSAL _> : : : ', proposal);
-      console.log('PROPOSAL OBJECT _> : : : ', proposalObject);
+        const url = `${CLOUDFUNCTIONS_URL}update-proposals`;
+        const request = await fetch(url);
+        if (request.status !== 200) {
+          throw Error(`Error fetching ${url}: ${request.status} ${request.statusText}`);
+        }
+        console.log(Date(), 'Updated Proposals: ', request.status, request.statusText);
+      } catch (err) {
+        console.error(`${Date()} ${err}`);
+      }
     });
-    const url = `${CLOUDFUNCTIONS_URL}update-proposals`;
+}
 
-    const request = await fetch(url);
-    if (request.status !== 200) {
-      throw Error(`Error fetching ${url}: ${request.status} ${request.statusText}`);
-    }
-    console.log('Updated Proposals: ', request.status, request.statusText);
-});
+subscribeToCommons();
+subscribeToProposals();
 
 // arc.plugins({}, {subscribe: true, fetchAllData: true})
 //   .subscribe(async () => {
