ad2404c9091beee8deffe4add736ca9d8ca011a9
jelle <jellegerbrandy@gmail.com>
2020-06-02 13:17:56 +0200

    upgrade subgraph to v8.2 and arc.js to .39

---
+++ settings.js
@@ -0,0 +1,9 @@
+const graphHttpLink = 'https://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai';
+const graphwsLink = 'wss://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai';
+const CLOUDFUNCTIONS_URL = `https://us-central1-common-daostack.cloudfunctions.net/api/`;
+
+module.exports = {
+  graphHttpLink,
+  graphwsLink, 
+  CLOUDFUNCTIONS_URL
+};
