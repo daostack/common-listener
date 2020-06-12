5fcacc8de1bf02dc013c8bb1adba68ec6ec9e1b1
jelle <jellegerbrandy@gmail.com>
2020-06-12 10:11:36 +0200

    new endpoint

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const graphHttpLink = 'https://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai';
 const graphwsLink = 'wss://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai';
-const CLOUDFUNCTIONS_URL = `https://us-central1-common-daostack.cloudfunctions.net/graphql-graphql/`;
+const CLOUDFUNCTIONS_URL = `https://us-central1-common-daostack.cloudfunctions.net/graphql/`;
 
 module.exports = {
   graphHttpLink,
