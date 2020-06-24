150e9b0fbab6033716cd15ec7fb04864c7eeb426
jelle <jellegerbrandy@gmail.com>
2020-06-24 18:20:27 +0200

    tweak more deploy scripts

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,6 +1,7 @@
 ssh -i ~/.ssh/common-listener.pem ubuntu@ec2-3-23-162-232.us-east-2.compute.amazonaws.com << EOF
     set -e
     cd common-listener-staging
+    git pull
     git checkout staging
     git pull
     ./env/env.sh -stg
