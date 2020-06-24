6780d23acd1b96e619f48ddf371d3b9183533dc6
lmcmz <lmcmze@gmail.com>
2020-06-24 22:25:20 +1000

    Add deploy staging

--- deploy-stg.sh
+++ deploy-stg.sh
@@ -1,7 +1,7 @@
 ssh -i ~/.ssh/common-listener.pem ubuntu@ec2-3-23-162-232.us-east-2.compute.amazonaws.com << EOF
     set -e
-    cd common-listener
-    ./env/env.sh -prod
+    cd common-listener-staging
+    ./env/env.sh -stg
     git pull
     yarn
     yarn forever
