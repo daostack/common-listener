3382f2472089db7c91a388cba935a91846f1dedb
jelle <jellegerbrandy@gmail.com>
2020-06-24 18:18:00 +0200

    tweaks, use staging env

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 ssh -i ~/.ssh/common-listener.pem ubuntu@ec2-3-23-162-232.us-east-2.compute.amazonaws.com << EOF
     set -e
     cd common-listener
-    ./env/env.sh -prod
+    git checkout master
     git pull
+    ./env/env.sh -prod
     yarn
     yarn forever
 EOF
